fa93aa2aa26339e6eb8da0ecbe3f5275a8185ec8
Crawford-H <crawf.h2@gmail.com>
2021-04-05 17:59:42 -0400

    Started the server

---
+++ RockPaperScissors/src/sample/Connection.java
@@ -0,0 +1,12 @@
+package sample;
+
+import java.net.Socket;
+
+public class Connection implements Runnable{
+    private Socket socket;
+
+    @Override
+    public void run() {
+
+    }
+}
--- Server/src/sample/Main.java
+++ Server/src/sample/Main.java
@@ -6,14 +6,26 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
+    public static Server server;
+    public static Thread serverThread;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("RPS Server");
         primaryStage.setScene(new Scene(root, 300, 275));
+        server = new Server();
         primaryStage.show();
+        serverThread = new Thread(server);
+        serverThread.start();
+    }
+
+    @Override
+    public void stop() throws IOException {
+        server.stop();
     }
 
 
